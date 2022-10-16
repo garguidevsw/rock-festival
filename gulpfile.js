60b6f2024d20f8bfa4d62689573e8945099921c8
Julio Cesar Garcia Guillen <garguidevsw@gmail.com>
2022-10-16 00:31:54 -0500

    Add and config GULP

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const { src, dest, watch } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+const css = ( done ) => {
+    src('src/scss/**/*.scss')  // Identificar el archivo de sass
+        .pipe( sass() )  // Compilar
+        .pipe( dest('build/css') );  // Almacenarla en disco
+
+    done();  // Callback que avisa a gulp cuando se termina de ejecutar la función
+}
+
+
+const dev = ( done ) => {
+    watch('src/scss/**/*.scss', css);
+
+    done();
+}
+
+exports.css = css;
+exports.dev = dev;
