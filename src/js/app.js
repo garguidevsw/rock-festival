3de6b97ac17f51dd6b8a78a35ac906e149224450
Julio Cesar Garcia Guillen <garguidevsw@gmail.com>
2022-10-21 10:10:13 -0500

    Add ScrollSmooth

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const initApp = () => {
     crearGaleria();
+    scrollNav();
+}
+
+const scrollNav = () => {
+    const enlaces = document.querySelectorAll('.navegacion-principal a');
+
+    enlaces.forEach( enlace => {
+        enlace.addEventListener('click', (event) => {
+            event.preventDefault();
+            const seccionScroll = event.target.attributes.href.value;
+            const seccion= document.querySelector(seccionScroll);
+
+            seccion.scrollIntoView({
+                behavior: "smooth",
+            });
+        });
+    });
 }
 
 const crearGaleria = () => {
