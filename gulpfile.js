47626c110a5d8b332a93588ff113dfe543e2aa17
Julio Cesar Garcia Guillen <garguidevsw@gmail.com>
2022-10-22 09:10:05 -0500

    Add autoprefixer, cssnano, postcss, gulp-postcss

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ const { src, dest, watch, parallel } = require("gulp");
 // CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
 
 //Imagenes
 const cache = require('gulp-cache');
@@ -51,8 +55,11 @@ const versionAvif = ( done ) => {
 
 const css = ( done ) => {
     src('src/scss/**/*.scss')  // Identificar el archivo de sass
+        .pipe( sourcemaps.init() )
         .pipe( plumber() )
         .pipe( sass() )  // Compilar
+        .pipe( postcss([ autoprefixer(), cssnano() ]) )
+        .pipe( sourcemaps.write('.') ) 
         .pipe( dest('build/css') );  // Almacenarla en disco
 
     done();  // Callback que avisa a gulp cuando se termina de ejecutar la función
